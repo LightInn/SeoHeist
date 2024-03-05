4edfe158c5d3d29ab45bf93e1b9951e5a63d2e71
breval <breval2000@live.fr>
2024-03-05 02:05:07 +0100

    Update project configuration and package.json, add script for development, modify API key in main.js, implement function to pick random keywords, adjust article generation process, save articles with metadata to PocketBase. Update project configuration and package.json, add development script, modify API key in main.js, implement random keyword selection function, adjust article generation process, save articles with metadata to PocketBase.

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ const ProgressBar = require('console-progress-bar');
 
 const openai = new OpenAI({
     apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
-    // apiKey: "xxxx"
+    // apiKey: "****"
 });
 const pb = new PocketBase('https://stickman-api.lightin.io');
 
@@ -28,13 +28,37 @@ const pb = new PocketBase('https://stickman-api.lightin.io');
 
 const language = 'french'
 
+var keywords = ["stick", "duel", "medieval", "wars", "french", "drawing", "digital", "animation", "game",
+    "video", "2d", "3d", "stickman", "stickfigure", "nintendo", "youtube", "children", "kids", "funny", "fight", "ninja", "robot"];
 
-var keywords = ["stick duel medieval wars"];
+var usedKeywords = [];
+
+// function to pick 3 random keywords from the list, keywords should not be already picked together
+function pick3RandomKeywords(keywords) {
+    var result = ["stickman"];
+    var notFound = true;
+
+    while (notFound) {
+        // pick 2 random keywords
+        var random1 = Math.floor(Math.random() * keywords.length);
+        var random2 = Math.floor(Math.random() * keywords.length);
+
+        temporaryResult = [keywords[random1], keywords[random2]];
+
+        // check if the 3 keywords are not already picked together
+        if (!usedKeywords.includes(temporaryResult)) {
+            result = result.concat(temporaryResult);
+            usedKeywords.push(temporaryResult);
+            notFound = false;
+        }
+    }
+    return result;
+}
 
 
 async function generateArticle(keyword) {
 
-    const progressBar = new ProgressBar({ maxValue: 5 });
+    const progressBar = new ProgressBar({maxValue: 5});
 
 
     // 6 - genereate articles
@@ -47,12 +71,12 @@ async function generateArticle(keyword) {
         messages: [{
             role: 'user',
             content: "write a short and simple title for an article about \"" + keyword + "\". The output should be in " + language
-        }],
-        model: 'gpt-3.5-turbo',
+        }], model: 'gpt-3.5-turbo',
     });
 
-    progressBar.addValue(1);
+    console.log(chatCompletion.choices[0].message.content);
 
+    progressBar.addValue(1);
 
 
 // - write a metadata description for an article named #title. keep it under 150 characters
@@ -61,8 +85,7 @@ async function generateArticle(keyword) {
         messages: [{
             role: 'user',
             content: "write a metadata description for an article named \"" + chatCompletion.choices[0].message.content + "\". keep it under 150 characters. The output should be in " + language
-        }],
-        model: 'gpt-3.5-turbo',
+        }], model: 'gpt-3.5-turbo',
     });
 
     progressBar.addValue(1);
@@ -73,9 +96,8 @@ async function generateArticle(keyword) {
     const chatCompletion3 = await openai.chat.completions.create({
         messages: [{
             role: 'user',
-            content: "write an article about \"" + chatCompletion.choices[0].message.content + "\" that take in count the metadescription : \"" + chatCompletion2 + "\". subheadings should be in BBCode format. dont include the article title in the output. The output should be in " + language
-        }],
-        model: 'gpt-4-turbo-preview',
+            content: "write an article about \"" + chatCompletion.choices[0].message.content + "\" that take in count the metadescription : \"" + chatCompletion2 + "\". the article should be in html rich text format. dont include the article title in the output. dont include the markdown code backtick, output the html directly. dont include the html doctype or the head section. The output should be in " + language
+        }], model: 'gpt-4-turbo-preview',
     });
 
 
@@ -83,6 +105,17 @@ async function generateArticle(keyword) {
 // - generate image from #title
 
     // voir avec midjourney pour les images
+
+    const response = await openai.images.generate({
+        // model: "dall-e-3",
+        model: "dall-e-2",
+        // title = Image
+        prompt: chatCompletion.choices[0].message.content,
+        n: 1, // size: "1792x1024",
+        size: "256x256",
+    });
+
+    image_url = response.data[0].url;
 
 // 7 - create Article with keywords, content, title, image, image-meta, meta-description. use keywords as slug and tags
 
@@ -92,44 +125,36 @@ async function generateArticle(keyword) {
         title: chatCompletion.choices[0].message.content,
         metaDescription: chatCompletion2.choices[0].message.content,
         content: chatCompletion3.choices[0].message.content,
-        keywords: keyword,
-        slug: keyword,
-        tags: keyword
+        image: image_url
     }
 }
 
 async function main() {
 
+    //pick random keywords
+    var keywordsChoosed = pick3RandomKeywords(keywords);
 
-// 9 - repeat for all keywords
-    for (let keyword of keywords) {
+    console.log(keywordsChoosed);
 
+    // concatene keywords
+    var keyword = keywordsChoosed.join(' ');
 
-        var article = await generateArticle(keyword)
+    // 7 - generate article
+    var article = await generateArticle(keyword)
 
-        console.log(article)
+    // 8 - save article to pocketbase or Strapi
+    // PocketBase
 
+    const formData = new FormData();
+    formData.append('title', article.title);
+    formData.append('describe', article.metaDescription);
+    formData.append('content', article.content);
+    formData.append('url', article.title.replace(/[^a-zA-Z0-9]/g, '').replace(/\s+/g, '-').toLowerCase());
+    formData.append('image', article.image);
+    formData.append('tags', JSON.stringify(keywordsChoosed));
+    formData.append('publishedAt', new Date().toISOString());
 
-
-
-        // 8 - save article to pocketbase or Strapi
-        // PocketBase
-
-        // example create data
-        const data = {
-            "title": article.title,
-            "content": article.content,
-            "describe": article.metaDescription,
-            "slug": article.slug,
-            "keywords": article.keywords,
-        };
-
-        const record = await pb.collection('blog').create(data);
-
-
-    }
-
-
+    const record = await pb.collection('blog').create(formData);
 }
 
 main();
